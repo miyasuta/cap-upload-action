52a4326ee08fbd4746246a941eba3fad6a990f2a
Mio Yasutake <mio.fujita.01@gmail.com>
2025-06-05 05:08:39 +0900

    initial commit

---
+++ srv/cat-service.cds
@@ -0,0 +1,11 @@
+using my.bookshop as my from '../db/schema';
+
+service CatalogService {
+    @readonly entity Books as projection on my.Books;
+
+    @cds.persistence.skip
+    @odata.singleton
+    entity ExcelUpload {
+        excel: LargeBinary;
+    }
+}
---
+++ srv/cat-service.ts
@@ -0,0 +1,61 @@
+import cds, { entity } from '@sap/cds'
+import { Books, ExcelUpload } from '#cds-models/CatalogService'
+import { PassThrough } from 'stream'
+import * as XLSX from 'xlsx'
+
+export class CatalogService extends cds.ApplicationService {
+  init() {
+
+    this.on('PUT', ExcelUpload, async (req, next) => {
+      if (!req.data.excel) {
+        return next()
+      } 
+      else {
+        // var entity = req.headers.slug;
+        const streadm = new PassThrough()
+        let buffers: Buffer[] = []
+        req.data.excel.pipe(streadm)
+        await new Promise((resolve, reject) => {
+          streadm.on('data', (chunk) => {
+            buffers.push(chunk)
+          })
+          streadm.on('end', async () => {
+            const buffer = Buffer.concat(buffers)
+            const workbook = XLSX.read(buffer, { type: 'buffer' })
+            let data: object[] = []
+            const sheets = workbook.SheetNames
+            for (let i = 0; i < sheets.length; i++) {
+              const temp = XLSX.utils.sheet_to_json(
+                workbook.Sheets[sheets[i]], { rawNumbers: false, dateNF: 'yyyy-mm-dd' })
+
+              temp.forEach((res, index) => {
+                if (index === 0) {
+                  return
+                }
+                data.push(JSON.parse(JSON.stringify(res)))
+              })
+            }
+            if (data) {
+              const responseCall = await saveBooks(data)
+              if (responseCall == -1) {
+                req.error(400, 'Error while saving data')
+              } else {
+                resolve(req.notify(200, 'Upload Successful'))
+              }
+            }
+            
+          })
+        })
+
+      } 
+    })
+
+    async function saveBooks(data: object[]): Promise<any> {
+      const insertQuery = INSERT.into(Books).entries(data)
+      const insertResult = await cds.run(insertQuery)
+      return insertResult
+    }
+
+    return super.init()
+  }
+}
